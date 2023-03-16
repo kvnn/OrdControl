e682203cd7bfa072f271ed2380d0ce7406b1b072
kvnn <kriggen@gmail.com>
2023-03-16 08:02:04 -1000

    Fixes snapshot id

--- variables.tf
+++ variables.tf
@@ -17,15 +17,11 @@ variable "availability_zone" {
 
 variable "snapshot_id" {
   type = string
-  # march 12 2023 : data dir synced w/ bitcoind and ord
-  default = "snap-0e20148c69de271c8"
+  # march 15 2023 : data dir synced w/ bitcoind and ord
+  default = "snap-0cf743ff2aa517142"
 }
 
 variable "instance_type" {
   type = string
-  # Compute optimized, e.g. c6a.xlarge, would likely be better but I've found the
-  # disk logic in those instance types to be indeterministic and time consuming to program.
-  default = "t2.large" # ~$67 / month
-  # default = "c6a.xlarge" # ~ $110 / month      compute-optimized   4vCPU	8GB
-  # default = "x2gd.large"  # ~ $120 / month   memory-optimized    2vCPU	32GB
+  default = "t2.medium" # t2.large is stable; t2.medium is experimental
 }
